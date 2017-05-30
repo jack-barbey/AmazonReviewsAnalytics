97f1ce861c11609bea1e1764b321146e65bbae80
Peter Wiggin <pwiggin7@gmail.com>
2017-05-30 07:00:22 -0500

    pushed final version of job_without_cossim

--- instruments/job_without_cossim.py
+++ instruments/job_without_cossim.py
@@ -27,82 +27,28 @@ class Mega_MRJob(MRJob):
 
     # 1st review info from reviews file
     f1_line = json.loads(f1_str) # convert string to dictionary
-    reviewerID1, productID1, uniqueID1 = get_IDs(f1_line)
-    if uniqueID1:
+    reviewerID1, productID1 = get_IDs(f1_line)
+    if productID1:
       overall1, unixReviewTime1 = get_attrs(f1_line)
+      if overall1 and unixReviewTime1:
+        # Re-open each time in order to start at top of file
+        self.f2 = gzip.open("full_instruments2.json.gz", "r")
+        for f2_bytes in self.f2:
+          f2_line = json.loads(f2_bytes.decode())
+          reviewerID2, productID2 = get_IDs(f2_line)
 
-      # 1st review's product info from metadata
-      price1 = single_value_query(self.c, "price",
-          "products_instruments", productID1)
-      title1 = single_value_query(self.c, "title",
-          "products_instruments", productID1)
-      if title1:
-        title1_length = len(re.findall("\w+", title1))
-        pct_upper1 = min(len(re.findall("[A-Z]", title1)) /
-            len(re.findall("[a-zA-Z]", title1)), 1)
-      category1 = single_value_query(self.c, "salesCategory",
-          "products_instruments", productID1)
-      rank1 = single_value_query(self.c, "salesRank",
-          "products_instruments", productID1)
+          # Only compare different reviews for same product
+          if productID1 == productID2 and reviewerID1 != reviewerID2:
+            overall2, unixReviewTime2 = get_attrs(f2_line)
+            if overall2 and unixReviewTime2:
+              overall_diff = diff(overall1, overall2)
+              time_gap = diff(unixReviewTime1, unixReviewTime2)
+              day_gap = int(time_gap / (60 * 60 * 24)) # days between reviews
 
-      # Re-open each time in order to start at top of file
-      self.f2 = gzip.open("full_instruments2.json.gz", "r")
-      for f2_bytes in self.f2:
-        f2_line = json.loads(f2_bytes.decode())
-        reviewerID2, productID2, uniqueID2 = get_IDs(f2_line)
-        # Don't compare reviews for same product
-        if productID1 != productID2:
-          overall2, unixReviewTime2 = get_attrs(f2_line)
-
-          overall_diff = diff(overall1, overall2)
-          time_gap = diff(unixReviewTime1, unixReviewTime2)
-          day_gap = int(time_gap/86400) # number of days between reviews
-
-          if price1:
-            price2 = single_value_query(self.c, "price",
-                "products_instruments", productID2)
-
-          if title1:
-            title2 = single_value_query(self.c, "title",
-                "products_instruments", productID2)
-            if title2:
-              title2_length = len(re.findall("\w+", title2))
-              pct_upper2 = min(len(re.findall("[A-Z]", title2)) / \
-                  len(re.findall("[a-zA-Z]", title2)), 1)
-              title_len_diff = diff(title1_length, title2_length)
-              pct_upper_diff = int(100 * diff(pct_upper1, pct_upper2))
-
-          if category1 and rank1:
-            category2 = single_value_query(self.c, "salesCategory",
-                "products_instruments", productID2)
-            if category1 == category2:
-              rank2 = single_value_query(self.c, "salesRank",
-                  "products_instruments", productID2)
-
-          # Ranks are only comparable within a category
-          if category1 and category1 == category2:
-            if title_len_diff >= 0:
-              # Middle element is 1 if longer review is better, else 0
-              yield [0, int(rank2 > rank1), title_len_diff], 1
-            else:
-              yield [0, int(rank1 > rank2), -title_len_diff], 1
-
-            if day_gap >= 0 and day_gap <= 365:
-              yield [1, int(rank2 > rank1), day_gap], 1
-            elif day_gap < 0 and day_gap >= -365:
-              yield [1, int(rank1 > rank2), -day_gap], 1
-
-            if pct_upper_diff >= 0:
-              yield [2, int(rank2 > rank1), pct_upper_diff], 1
-            else:
-              yield [2, int(rank1 > rank2), -pct_upper_diff], 1
-
-          if price1 and price2 and overall_diff:
-            if price1 > price2:
-              # Interpretation: one product is X% the price, got Y more stars
-              yield [3, int(100 * price2 / price1), overall_diff], 1
-            else:
-              yield [3, int(100 * price1 / price2), -overall_diff], 1
+              if day_gap > 0 and day_gap <= 365:
+                yield [0, day_gap, overall_diff], 1
+              elif day_gap < 0 and day_gap >- -365:
+                yield [0, -day_gap, -overall_diff], 1
 
       self.f2.close() # close, then re-open later at top of file
 
@@ -129,19 +75,15 @@ def diff(val1, val2):
 
 # SQLite query to get reviewerID, productID, and a concatenation
 def get_IDs(f_line):
-  if "reviewerID" in f_line:
-    reviewerID = f_line["reviewerID"]
-  else:
-    reviewerID = None
-  if "asin" in f_line:
-    productID = f_line["asin"]
-  else:
-    productID = None
-  if reviewerID != None and productID != None:
-    uniqueID = f_line["reviewerID"] + f_line["asin"]
-  else:
-    uniqueID = None
-  return reviewerID, productID, uniqueID
+    if "reviewerID" in f_line:
+        reviewerID = f_line["reviewerID"]
+    else:
+        reviewerID = None
+    if "asin" in f_line:
+        productID = f_line["asin"]
+    else:
+        productID = None
+    return reviewerID, productID
 
 # Get necessary attributes of review: overall (e.g. 5 stars), timestamp
 def get_attrs(f_line):
