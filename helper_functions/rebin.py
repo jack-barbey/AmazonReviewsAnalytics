04b954fca974d8a88123f23033ef187bcbfb90f9
flgasaway <flgarch@gmail.com>
2017-05-30 08:37:27 -0500

    More comments

--- helper_functions/rebin.py
+++ helper_functions/rebin.py
@@ -1,9 +1,18 @@
 import mrjob.job
 import sys
 
-
+'''
+Usage: python3 rebin.py <old_data.csv> > <rebined_data.csv>
+'''
 
 class reverse(mrjob.job.MRJob):
+    '''
+    Takes in a csv file containing output from a job that contains percent ratios greater than 100%
+    and inverts the percents. For example, 125% becomes 80%.
+    Produces a new CSV file.
+    This enhances binning by keeping all percents between 0 and 100, instead of 0 and infinity.
+    '''
+    
     # This protocol lets sets be used
     INTERNAL_PROTOCOL = mrjob.protocol.PickleProtocol
 
@@ -17,7 +26,7 @@ class reverse(mrjob.job.MRJob):
         nums[0] = int(nums[0])
         nums[3] = int(nums[3])
 
-
+        # Inverts ratios 
         if nums[0] == 5:
             new = int( 100/ (nums[1] / 100))
             yield [5, new, nums[2]], nums[3]
